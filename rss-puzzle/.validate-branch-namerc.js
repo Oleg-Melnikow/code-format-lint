cfbb3254eeff0acc691722410a12a369609b7fea
Oleg-Melnikow <oleg.melnikau@gmail.com>
2024-03-07 17:47:08 +0300

    chore: start rss-puzzle task and create config

---
+++ rss-puzzle/.validate-branch-namerc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  pattern: '^(init|feat|fix|refactor|docs|chore)/RSS-RZ-\\d_\\w+$',
+  errorMsg:
+    'The branch you`re trying to push doesn`t match the expected convention, please rename it!',
+};
