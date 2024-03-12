48c6e1b6245c7ad1a8fb6be0fdd9112d911af045
Oleg-Melnikow <oleg.melnikau@gmail.com>
2024-03-12 12:29:00 +0300

    feat: add header with logout button on main page

--- rss-puzzle/src/app/pages/mainPage/mainPage.ts
+++ rss-puzzle/src/app/pages/mainPage/mainPage.ts
@@ -1,9 +1,21 @@
 import createElement from 'helpers/createElement';
+import { BaseClass } from 'types/interfaces';
+import Header from './header/header';
 import './mainPage.scss';
 
 class MainPage {
+  callback: (padeId: string) => void;
+
+  header: BaseClass;
+
+  constructor(callback: (padeId: string) => void) {
+    this.callback = callback;
+    this.header = new Header(this.callback);
+  }
+
   draw(root: HTMLElement): void {
-    const startPage = createElement('div', { class: 'main-page' }, 'Main Page');
+    const startPage = createElement('div', { class: 'main-page' });
+    this.header.draw(startPage);
     root.append(startPage);
   }
 }
