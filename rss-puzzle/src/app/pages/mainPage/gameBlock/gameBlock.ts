772632f94951273d904a14852e018e4b8d91632d
Oleg-Melnikow <oleg.melnikau@gmail.com>
2024-03-13 20:01:52 +0300

    feat: add methods in class gameBlock for render block with words

--- rss-puzzle/src/app/pages/mainPage/gameBlock/gameBlock.ts
+++ rss-puzzle/src/app/pages/mainPage/gameBlock/gameBlock.ts
@@ -6,24 +6,21 @@ import wordsRandomOrder from 'helpers/wordsRandomOrder';
 import CustomButton from 'components/customButton/customButton';
 
 class GameBlock {
+  containerResults: HTMLElement;
+
+  constructor() {
+    this.containerResults = createElement('div', {
+      class: 'results-container',
+    });
+  }
+
   async draw(root: HTMLElement): Promise<void> {
     await getRounds();
+    const { containerResults } = this;
     const container = createElement('div', { class: 'game-container' });
+
     const cardsBlock = createElement('div', { class: 'source-block' });
-    const resultBlock = createElement('div', { class: 'result-block' });
-
-    if (initialState.roundsData) {
-      const { rounds } = initialState.roundsData;
-      const wordArray = rounds[0].words[0].textExample.split(' ');
-
-      initialState.changeGameStatus({ count: 0, limit: wordArray.length - 1 });
-
-      wordsRandomOrder(wordArray).forEach((word, posiiton) => {
-        const card = new CardWord(word);
-        card.draw(resultBlock, 'result', posiiton);
-        card.draw(cardsBlock, 'source', posiiton);
-      });
-    }
+    const resultBlock = this.createResultBlock();
 
     const button = new CustomButton(
       {
@@ -31,13 +28,47 @@ class GameBlock {
         attributes: { class: 'button continue-btn', disabled: 'disabled' },
         textContent: 'Continue',
       },
-      this.continueGame.bind(this)
+      this.continueGame
     );
 
-    [resultBlock, cardsBlock, button.render()].forEach((item) =>
+    this.renderBlockWords(resultBlock, cardsBlock);
+    containerResults.append(resultBlock);
+
+    [containerResults, cardsBlock, button.render()].forEach((item) =>
       container.append(item)
     );
+
     root.append(container);
+  }
+
+  renderBlockWords(
+    result: HTMLElement | null,
+    source: HTMLElement | null
+  ): void {
+    if (initialState.currentSentence) {
+      const { textExample } = initialState.currentSentence;
+      const wordArray = textExample.split(' ');
+      console.log(textExample);
+      initialState.changeGameStatus({ count: 0, limit: wordArray.length - 1 });
+
+      wordsRandomOrder(wordArray).forEach((word, position) => {
+        const card = new CardWord(word);
+        if (result) {
+          card.draw(result, 'result', position);
+        }
+        if (source) {
+          card.draw(source, 'source', position);
+        }
+      });
+    }
+  }
+
+  createResultBlock(): HTMLElement {
+    const resultBlockId = initialState.currentSentence?.id;
+    return createElement('div', {
+      class: 'result-block',
+      'data-result_id': `${resultBlockId}`,
+    });
   }
 
   continueGame(): void {
